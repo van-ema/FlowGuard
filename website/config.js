a79721414aeca3f44e0daf046afbc1d2ed75f43b
emanuele vannacci <emanuele.vannacci@gmail.com>
2026-07-29 16:22:57 +0200

    Remove embedded landing page

--- website/config.js
+++
@@ -1,5 +0,0 @@
-window.FlowguardSupabase = {
-  url: "https://lgkqjkxxkvxaztdibiew.supabase.co",
-  publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxna3Fqa3h4a3Z4YXp0ZGliaWV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MDUzMjcsImV4cCI6MjEwMDQ4MTMyN30.NKngmkFUre9d9CTCBdXQURnea_Z6ZYpf0gxwD-LBRjU",
-  table: "demo_requests",
-};
